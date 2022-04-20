a737d7143a302b6138e4ff0751f17562aeeaa01e
luciomelis <luciomelis1992@gmail.com>
2022-04-20 17:03:48 +0200

    header completato

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,52 @@
+<template>
+  <header>
+    <div class="container-header">
+      <div class="container-img">
+        <img src="@/assets/Logonetflix.png" alt="Logo" />
+      </div>
+      <div class="container-input">
+        <input type="text" placeholder="Ricerca Film-Serie Tv" />
+        <button>Cerca</button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container-header {
+  background-color: black;
+  padding: 20px 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  .container-img {
+    width: 200px;
+    img {
+      width: 100%;
+    }
+  }
+  .container-input {
+    border: 2px solid red;
+    padding: 5px;
+    input {
+      padding: 5px 10px;
+    }
+    button {
+      margin-left: 5px;
+      padding: 5px 10px;
+      border-radius: 5px;
+      background-color: red;
+      font-weight: bold;
+      color: white;
+    }
+  }
+}
+</style>
